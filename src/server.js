3d4c8568c03c16142cb2a6256a3667c34b0b48d3
Saheed Ajibulu <stunnafrsh007@gmail.com>
2020-01-28 18:48:13 +0100

    fix startup issue in production build

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
 
-app.listen(port);
+app.listen(port, console.log(`Application is running on port ${port}`));
